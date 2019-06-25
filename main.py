951f9d24e8c66ee64234e0a4b324d9a13aaa9f77
Lorainen123 <48633260+Lorainen123@users.noreply.github.com>
2019-06-25 17:21:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ while sw==True:
     if x==1:
         sw1 = True
         while sw1==True:
-           print('Ingrese el valor de voltaje entre el rango de 14.5 y 18.6')
-           v = input()
-           n = excel.main(float(v),0)
-	   n = int(n)
-           mcpras.set_value(n)
-           print('Para salir ingrese 3, para seguir ajustando 1')
-           y = input()
-           if y==3:
-              sw1=False
-           elif y==1:
-               sw1= True
+              print('Ingrese el valor de voltaje entre el rango de 14.5 y 18.6')
+              v = input()
+              n = excel.main(float(v),0)
+	      n = int(n)
+              mcpras.set_value(n)
+              print('Para salir ingrese 3, para seguir ajustando 1')
+              y = input()
+              if y==3:
+                 sw1=False
+              elif y==1:
+                 sw1= True
     if x==2:
         excel.main(0,1)
         print('Descargado')
